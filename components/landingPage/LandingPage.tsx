a5eab8d512a925e098f7c0fc94d00fabd7fa4cc1
thatsddr <domenicodiruocco01@gmail.com>
2021-02-17 16:16:11 +0100

    New Component For The Future Landing Page

---
+++ components/landingPage/LandingPage.tsx
@@ -0,0 +1,10 @@
+import styles from "../../styles/LandingPage.module.css";
+
+export default function LandingPage() {
+	return (
+		<div className={styles.container}>
+			<h1>Hello, Welcome to GoodBuy</h1>
+			<p>It looks empty now but it won't in the future 😀</p>
+		</div>
+	);
+}
