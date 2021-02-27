bec626b58d267ec75087491c46394c1cafe08d57
thatsddr <domenicodiruocco01@gmail.com>
2021-02-27 16:06:45 +0100

    updated UIContext variables names

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -3,15 +3,13 @@ import { useContext } from "react";
 import { UIContext } from "../../lib/context/UIContext";
 
 export default function LandingPage() {
-	const { isDarkMode, toggleDarkMode } = useContext(UIContext);
+	const { colorMode, toggleColorMode } = useContext(UIContext);
 	return (
 		<div className={[styles.container, "bg"].join(" ")}>
-			<h1 className={"primary-text"}>Hello, Welcome to GoodBuy</h1>
+			<h1 className={"secondary-text"}>Hello, Welcome to GoodBuy</h1>
 			<p className={"primary-text"}>It looks empty now but it won't in the future 😀</p>
-			<p className={"primary-text"}>
-				The current theme is dark: {isDarkMode ? <strong>True</strong> : <strong>False</strong>}
-			</p>
-			<button onClick={toggleDarkMode}>Toggle</button>
+			<p className={"primary-text"}>The current color theme is: {<b>{colorMode}</b>}</p>
+			<button onClick={toggleColorMode}>Toggle</button>
 		</div>
 	);
 }
