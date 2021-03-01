b317443a8e42d668d072e9bffd181059c0b37ea0
thatsddr <domenicodiruocco01@gmail.com>
2021-03-01 18:45:53 +0100

    implemented tailwind classes

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -6,10 +6,15 @@ export default function LandingPage() {
 	const { colorMode, toggleColorMode } = useContext(UIContext);
 	return (
 		<div className={[styles.container, "bg-white dark:bg-black"].join(" ")}>
-			<h1 className="text-tufts-blue dark:text-red-crayola">Hello, Welcome to GoodBuy</h1>
+			<h1 className="text-3xl font-bold text-tufts-blue dark:text-red-crayola">
+				Hello, Welcome to GoodBuy
+			</h1>
 			<p className="text-black dark:text-white">It looks empty now but it won't in the future 😀</p>
 			<p className="text-black dark:text-white">The current color theme is: {<b>{colorMode}</b>}</p>
-			<button onClick={toggleColorMode} className="text-black dark:text-white">
+			<button
+				onClick={toggleColorMode}
+				className="text-lg px-3 text-white bg-tufts-blue rounded-full hover:bg-white hover:text-tufts-blue dark:bg-red-crayola dark:hover:bg-black dark:hover:text-red-crayola"
+			>
 				Toggle
 			</button>
 		</div>
