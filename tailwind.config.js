2b5e5f9153f9c25826886f62651d74234dfe4ddc
thatsddr <domenicodiruocco01@gmail.com>
2021-03-01 18:31:16 +0100

    added tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+	darkMode: "class", // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				"red-crayola": "#ef3054",
+				"tufts-blue": "#0197f6"
+			}
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
