b831d7bc4c3e8f934a468325c56617b6478ea369
thatsddr <domenicodiruocco01@gmail.com>
2021-03-01 18:31:55 +0100

    updated css classes

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -5,11 +5,13 @@ import { UIContext } from "../../lib/context/UIContext";
 export default function LandingPage() {
 	const { colorMode, toggleColorMode } = useContext(UIContext);
 	return (
-		<div className={[styles.container, "bg"].join(" ")}>
-			<h1 className={"secondary-text"}>Hello, Welcome to GoodBuy</h1>
-			<p className={"primary-text"}>It looks empty now but it won't in the future 😀</p>
-			<p className={"primary-text"}>The current color theme is: {<b>{colorMode}</b>}</p>
-			<button onClick={toggleColorMode}>Toggle</button>
+		<div className={[styles.container, "bg-white dark:bg-black"].join(" ")}>
+			<h1 className="text-tufts-blue dark:text-red-crayola">Hello, Welcome to GoodBuy</h1>
+			<p className="text-black dark:text-white">It looks empty now but it won't in the future 😀</p>
+			<p className="text-black dark:text-white">The current color theme is: {<b>{colorMode}</b>}</p>
+			<button onClick={toggleColorMode} className="text-black dark:text-white">
+				Toggle
+			</button>
 		</div>
 	);
 }
