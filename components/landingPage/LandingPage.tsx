0ae1910f4af5d05422815f5851b018ab3ffcee0a
thatsddr <domenicodiruocco01@gmail.com>
2021-03-03 18:06:48 +0100

    generalized styles

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -1,18 +1,16 @@
-import styles from "../../styles/LandingPage.module.css";
 import { useContext } from "react";
 import { UIContext } from "../../lib/context/UIContext";
 
 export default function LandingPage() {
 	const { colorMode, toggleColorMode } = useContext(UIContext);
 	return (
-		<div className={`${styles.container} bg-white dark:bg-black`}>
+		<div
+			className={`flex flex-col bg-white dark:bg-black min-h-screen justify-center items-center space-y-3`}
+		>
 			<h1 className="text-3xl font-bold colorful-text">Hello, Welcome to GoodBuy</h1>
 			<p className="normal-text">It looks empty now but it won't in the future 😀</p>
 			<p className="normal-text">The current color theme is: {<b>{colorMode}</b>}</p>
-			<button
-				onClick={toggleColorMode}
-				className="text-lg px-3 colorful-background text-white rounded-full ring-2 ring-primary dark:ring-secondary"
-			>
+			<button onClick={toggleColorMode} className="text-lg px-5 colorful-button text-white w-auto">
 				Toggle
 			</button>
 		</div>
