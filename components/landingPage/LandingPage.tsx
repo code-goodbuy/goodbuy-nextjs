e417dda4d568e57bfd0ecae221ab67091bb3d95f
thatsddr <domenicodiruocco01@gmail.com>
2021-03-03 17:23:42 +0100

    refactored styles

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -6,14 +6,12 @@ export default function LandingPage() {
 	const { colorMode, toggleColorMode } = useContext(UIContext);
 	return (
 		<div className={`${styles.container} bg-white dark:bg-black`}>
-			<h1 className="text-3xl font-bold text-primary dark:text-secondary">
-				Hello, Welcome to GoodBuy
-			</h1>
-			<p className="text-black dark:text-white">It looks empty now but it won't in the future 😀</p>
-			<p className="text-black dark:text-white">The current color theme is: {<b>{colorMode}</b>}</p>
+			<h1 className="text-3xl font-bold colorful-text">Hello, Welcome to GoodBuy</h1>
+			<p className="normal-text">It looks empty now but it won't in the future 😀</p>
+			<p className="normal-text">The current color theme is: {<b>{colorMode}</b>}</p>
 			<button
 				onClick={toggleColorMode}
-				className="text-lg px-3 text-white bg-primary rounded-full hover:bg-white hover:text-primary dark:bg-secondary dark:hover:bg-black dark:hover:text-secondary"
+				className="text-lg px-3 colorful-background text-white rounded-full ring-2 ring-primary dark:ring-secondary"
 			>
 				Toggle
 			</button>
