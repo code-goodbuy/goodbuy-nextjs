86aaf8f11639c49b5ba86c6da0abf1b3f632c1fb
thatsddr <domenicodiruocco01@gmail.com>
2021-03-04 11:12:11 +0100

    added simple footer

---
+++ components/common/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+	return (
+		<div className="text-center bg-black text-white dark:bg-white dark:text-black">
+			<p className="py-2">Copyright © 2021 GoodBuy</p>
+		</div>
+	);
+}
--- components/common/Layout.tsx
+++ components/common/Layout.tsx
@@ -1,11 +1,13 @@
 import { ReactChildrenType } from "../../lib/types/ReactChildrenType";
 import Header from "./Header";
+import Footer from "./Footer";
 
 const Layout = ({ children }: ReactChildrenType) => {
 	return (
 		<>
 			<Header />
 			{children}
+			<Footer />
 		</>
 	);
 };
