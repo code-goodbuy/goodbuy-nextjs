b7a1e97af0f271fc6aacba85857e7f2c200d300e
thatsddr <domenicodiruocco01@gmail.com>
2021-03-04 17:48:48 +0100

    added min height config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,13 @@ module.exports = {
 			colors: {
 				"primary": "#0197f6",
 				"secondary": "#ef3054"
+			},
+			minHeight: {
+				"0": "0",
+				"1/4": "25vh",
+				"1/2": "50vh",
+				"3/4": "75vh",
+				"full": "100vh"
 			}
 		}
 	},
