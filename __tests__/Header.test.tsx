f4777779d24796ab142c0c4c656f4bfa4af9e4aa
thatsddr <domenicodiruocco01@gmail.com>
2021-03-06 14:46:48 +0100

    simple test files to check that the components are rendered properly

---
+++ __tests__/Header.test.tsx
@@ -0,0 +1,14 @@
+import { render, act, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import Header from "../components/common/Header";
+
+describe("Test the Header", () => {
+	it("should change the color mode", async () => {
+		const { getByTestId } = render(<Header />);
+		const switcher = getByTestId("colorSwitcher");
+		await act(async () => {
+			fireEvent.click(switcher);
+		});
+		expect(switcher).toHaveTextContent("Switch to Dark Theme");
+	});
+});
