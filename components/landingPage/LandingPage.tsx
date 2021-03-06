391fa49223ed829e846df16d05a34aa8a90ff167
thatsddr <domenicodiruocco01@gmail.com>
2021-03-06 15:55:53 +0100

    added About component

--- components/landingPage/LandingPage.tsx
+++ components/landingPage/LandingPage.tsx
@@ -1,10 +1,11 @@
+import About from "./About";
 import Mission from "./Mission";
 import Welcome from "./Welcome";
 
 export default function LandingPage() {
 	return (
 		<>
-			<Welcome /> <Mission />
+			<Welcome /> <Mission /> <About />
 		</>
 	);
 }
