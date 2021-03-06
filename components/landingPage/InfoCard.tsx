ad1fa70a2d845001120dabc63ba6ba14e8148ed7
thatsddr <domenicodiruocco01@gmail.com>
2021-03-06 12:04:08 +0100

    created helper component

---
+++ components/landingPage/InfoCard.tsx
@@ -0,0 +1,58 @@
+interface InfoCardType {
+	number: number;
+	unit: string;
+	text: string;
+	url: string;
+}
+export default function InfoCard({ number, unit, text, url }: InfoCardType) {
+	/**
+	 * Displays the amount of something since the beginning of the year.
+	 *
+	 * Props:
+	 * number: number, [the year estimate number]
+	 * unit, string, [unit of measurement]
+	 * text, string, [test to display]
+	 * url: stirng [the source URL]
+	 */
+	function dayOfTheYear() {
+		/**
+		 * returns the current day of the year (1-366)
+		 */
+		return (
+			(Date.UTC(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()) -
+				Date.UTC(new Date().getFullYear(), 0, 0)) /
+			24 /
+			60 /
+			60 /
+			1000
+		);
+	}
+	function isLeapYear(year: number) {
+		/**
+		 * checks if a year has 366 days
+		 */
+		return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+	}
+	function getPartialNumber(number: number) {
+		/**
+		 * returns the proportion of the total number like so [number : days of the year = x : current day]
+		 */
+		//the precision is set to the hour
+		return (
+			Math.floor(dayOfTheYear() * number * 24 + new Date().getHours()) /
+			(isLeapYear(new Date().getFullYear()) ? 365 * 24 : 366 * 24)
+		);
+	}
+	const partialNumber = parseInt(getPartialNumber(number).toFixed(0)).toLocaleString();
+	return (
+		<div className="rounded-2xl ring-2 ring-primary dark:ring-secondary p-5 w-75 md:w-50 lg:w-30 mt-10">
+			<h2 className="text-2xl font-bold colorful-text">
+				{partialNumber} {unit}
+			</h2>
+			<p className="normal-text mb-8">{text}</p>
+			<a className="colorful-button cursor-pointer" href={url}>
+				Learn More
+			</a>
+		</div>
+	);
+}
