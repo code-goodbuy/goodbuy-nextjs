8e8eb5262ae6988430ec494f247349c91fe5470c
thatsddr <domenicodiruocco01@gmail.com>
2021-03-06 14:47:38 +0100

    modified color mode switcher

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -3,20 +3,25 @@ import { useContext } from "react";
 import { UIContext } from "../../lib/context/UIContext";
 
 export default function Header() {
-	const { showMenu, toggleShowMenu, toggleColorMode } = useContext(UIContext);
+	const { showMenu, toggleShowMenu, toggleColorMode, colorMode } = useContext(UIContext);
 	return (
 		<header className="fixed inset-x-0 normal-bg border-b-2 colorful-border z-50">
 			<div className="normal-text flex-horizontal justify-between items-center px-4 py-1">
 				<h2 className="text-3xl lg:text-4xl colorful-text font-bold">GoodBuy</h2>
-				<button onClick={toggleShowMenu} className="sm:hidden colorful-button px-5">
+				<button
+					onClick={toggleShowMenu}
+					className="sm:hidden colorful-button px-5"
+					data-testid="menu"
+				>
 					Menu
 				</button>
 				<div className="space-x-4 hidden sm:flex flex-row">
 					<p
 						onClick={toggleColorMode}
+						data-testid="colorSwitcher"
 						className="hover:text-primary dark:hover:text-secondary cursor-pointer"
 					>
-						Toggle Dark Mode
+						{colorMode === "dark" ? "Switch to Light Theme" : "Switch to Dark Theme"}
 					</p>
 					<p className="hover:text-primary dark:hover:text-secondary">
 						<Link href="#mission">Our Mission</Link>
@@ -31,8 +36,12 @@ export default function Header() {
 			</div>
 			{showMenu && (
 				<div className="sm:hidden flex-vertical items-center space-y-4 mb-4 normal-text z-50">
-					<p onClick={toggleColorMode} className="hover:text-primary dark:hover:text-secondary">
-						Toggle Dark Mode
+					<p
+						onClick={toggleColorMode}
+						data-testid="colorSwitcher"
+						className="hover:text-primary dark:hover:text-secondary"
+					>
+						{colorMode === "dark" ? "Switch to Light Theme" : "Switch to Dark Theme"}
 					</p>
 					<p className="hover:text-primary dark:hover:text-secondary">
 						<Link href="#mission">Our Mission</Link>
