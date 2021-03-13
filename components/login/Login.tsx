0cfd59c583ade72193e133cf6c9ad61ba6a9a266
thatsddr <domenicodiruocco01@gmail.com>
2021-03-13 12:17:24 +0100

    added some style to the login component

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../lib/context/AuthContext";
 
 export default function Login() {
@@ -6,6 +6,7 @@ export default function Login() {
 	 * Main Login / Register component
 	 */
 	const { isAuthenticating, changeIsAuthenticating } = useContext(AuthContext);
+	const [action, setAction] = useState<"login" | "sign-up">("login");
 
 	useEffect(() => {
 		changeIsAuthenticating && changeIsAuthenticating(true);
@@ -16,8 +17,28 @@ export default function Login() {
 	}, [isAuthenticating]);
 
 	return (
-		<div className="min-h-full normal-bg">
-			<p className="pt-48 normal-text">Currently {isAuthenticating ? "yes" : "no"}</p>
+		<div className="min-h-full normal-bg flex flex-col justify-center items-center">
+			<div className="rounded-md ml-8 mr-8 normal-bg ring-2 ring-primary dark:ring-secondary min-h-3/4 w-90 flex flex-col">
+				<div className="flex flex-row">
+					<div
+						className={"action-selector rounded-tl-md" + (action === "sign-up" && " colorful-bg-2")}
+						onClick={() => {
+							setAction("login");
+						}}
+					>
+						Login
+					</div>
+					<div
+						className={"action-selector rounded-tr-md" + (action === "login" && " colorful-bg-2")}
+						onClick={() => {
+							setAction("sign-up");
+						}}
+					>
+						Sign Up
+					</div>
+				</div>
+				<div></div>
+			</div>
 		</div>
 	);
 }
