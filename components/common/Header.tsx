19562a0eddcb9854e5d4c67d89a638c607665fa2
thatsddr <domenicodiruocco01@gmail.com>
2021-03-13 11:45:33 +0100

    clicking on the goodbuy logo redirects to the landing page

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -2,26 +2,30 @@ import Image from "next/image";
 import { useContext } from "react";
 import { UIContext } from "../../lib/context/UIContext";
 import NavLinks from "./NavLinks";
+import Link from "next/link";
 
 export default function Header() {
 	/**
 	 * Responsive Header Component
 	 */
 	const { showMenu, toggleShowMenu, colorMode } = useContext(UIContext);
+
 	return (
 		<header className="fixed inset-x-0 normal-bg border-b-2 colorful-border z-50 p-2">
 			<div className="normal-text flex-horizontal justify-between items-center px-4 py-1">
-				<div className="flex flex-row items-center">
-					<Image
-						src={colorMode === "dark" ? "/pics/darkLogo.png" : "/pics/lightLogo.png"}
-						layout="fixed"
-						height={40}
-						width={40}
-						alt="logo"
-					/>
+				<Link href="/">
+					<div className="flex flex-row items-center cursor-pointer">
+						<Image
+							src={colorMode === "dark" ? "/pics/darkLogo.png" : "/pics/lightLogo.png"}
+							layout="fixed"
+							height={40}
+							width={40}
+							alt="logo"
+						/>
 
-					<h2 className="ml-3 text-3xl lg:text-4xl colorful-text font-bold">GoodBuy</h2>
-				</div>
+						<h2 className="ml-3 text-3xl lg:text-4xl colorful-text font-bold">GoodBuy</h2>
+					</div>
+				</Link>
 				<button
 					onClick={toggleShowMenu}
 					className="md:hidden colorful-button px-5"
