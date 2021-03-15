9649da73409fe608af00178303b9fecfc7b7f1ef
thatsddr <domenicodiruocco01@gmail.com>
2021-03-15 13:03:43 +0100

    added some logic to the login form

--- components/login/LoginForm.tsx
+++ components/login/LoginForm.tsx
@@ -1,23 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { updateWithoutSpaces, checkEmail } from "./helperFunctions";
 export default function LoginForm() {
 	const [email, setEmail] = useState<string>("");
+	const [isValidEmail, setIsValidEmail] = useState<boolean>(true);
 	const [password, setPassword] = useState<string>("");
+
+	useEffect(() => {
+		/**
+		 * Every time the email is updated, it checks if it is valid
+		 */
+		checkEmail(setIsValidEmail, email);
+	}, [email]);
+
 	return (
 		<form
 			onSubmit={(e) => e.preventDefault()}
 			id="login-form"
-			className="flex flex-col justify-center items-center space-y-8 my-14"
+			className="flex flex-col justify-center items-center my-14"
 		>
+			{!isValidEmail && <label className="error-label">This email is not valid</label>}
 			<input
-				type="email"
+				type="text"
 				placeholder="email"
-				onChange={(e) => setEmail(e.target.value)}
+				onChange={(e) => updateWithoutSpaces(setEmail, e.target.value)}
+				value={email || ""}
 				className="field focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
 			/>
 			<input
 				type="password"
 				placeholder="password"
-				onChange={(e) => setPassword(e.target.value)}
+				onChange={(e) => updateWithoutSpaces(setPassword, e.target.value)}
+				value={password || ""}
 				className="field focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
 			/>
 			<button type="submit" form="login-form" className="colorful-button mb-10">
