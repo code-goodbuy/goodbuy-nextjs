b35e52652c29e98ffc5047f0a22d44f72794d792
thatsddr <domenicodiruocco01@gmail.com>
2021-03-16 10:44:31 +0100

    deleted unused dependency

--- lib/context/AuthContext.tsx
+++ lib/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import { ReactChildrenType } from "../types/ReactChildrenType";
 
 interface AuthType {
