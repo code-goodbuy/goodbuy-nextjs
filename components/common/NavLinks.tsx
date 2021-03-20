60728cdeac403064298e228319f83d26bfc819a6
jwdotpark <jwdotpark@me.com>
2021-03-20 16:13:48 +0100

    fixed nested DOM validation complain

--- components/common/NavLinks.tsx
+++ components/common/NavLinks.tsx
@@ -17,9 +17,9 @@ export default function NavLinks({ className }: { className: string }) {
 			>
 				{colorMode === "dark" ? "Switch to Light Theme" : "Switch to Dark Theme"}
 			</p>
-			<p className="hover:text-primary dark:hover:text-secondary">
+			<span className="hover:text-primary dark:hover:text-secondary">
 				<ScannerPage />
-			</p>
+			</span>
 			<p className="hover:text-primary dark:hover:text-secondary">
 				<Link href="#mission">Our Mission</Link>
 			</p>
