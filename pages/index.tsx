590db1c16358b0e706e4f7f16d8dfa45aa7a2ab3
thatsddr <domenicodiruocco01@gmail.com>
2021-03-26 12:12:14 +0100

    added different view for logged-in users

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import { AuthContext } from "../lib/context/AuthContext";
 import Meta from "../components/common/Meta";
 import LandingPage from "../components/landingPage/LandingPage";
 
@@ -5,6 +7,15 @@ export default function Home() {
 	/**
 	 * Renders The Landing Page
 	 */
+	const { isLoggedIn, userInfo } = useContext(AuthContext);
+	if (isLoggedIn) {
+		return (
+			<>
+				<Meta title={"GoodBuy | You are logged in"} />
+				<div>{userInfo?.email}</div>
+			</>
+		);
+	}
 	return (
 		<>
 			<Meta title={"GoodBuy | Welcome to GoodBuy"} />
