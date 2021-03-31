5534e1b4641dc30faabe59a33cff69e4b3dcba4f
thatsddr <domenicodiruocco01@gmail.com>
2021-03-31 15:33:23 +0200

    check if the user is logged in

---
+++ pages/api/check.ts
@@ -0,0 +1,36 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getJWTCookie } from "../../lib/apiFunctions/responseHelpers";
+import { decodeJWT } from "../../lib/apiFunctions/jwtHelpers";
+
+export const config = {
+	api: {
+		bodyParser: false //don't parse the whole request, so we can forward it to the backend
+	}
+};
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+	return new Promise((resolve, reject): void => {
+		if (req.url === undefined) {
+			res.status(409).json({ message: "error" });
+			reject();
+		} else {
+			const token = getJWTCookie(req, res);
+			if (token && token !== "") {
+				let validJWT = false;
+				let data;
+				try {
+					data = decodeJWT(token);
+					validJWT = true;
+				} catch {
+					validJWT = false;
+				}
+				if (validJWT) {
+					res.status(200).json({ message: "logged", email: data?.email });
+					resolve("ok");
+				}
+			}
+			res.status(200).json({ message: "not logged" });
+			resolve("ok");
+		}
+	});
+};
