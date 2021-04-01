9c9c7a28b09c1e96e8369b069a2b8052bc62e959
jwdotpark <jwdotpark@me.com>
2021-04-01 22:52:53 +0200

    removed unused dependencies

--- __tests__/Scanner.test.tsx
+++ __tests__/Scanner.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, act, fireEvent, getByTestId } from "@testing-library/react";
+import { render, screen, fireEvent} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ScannerPage from "../components/utility/Scanner";
 // Mock Service Worker
