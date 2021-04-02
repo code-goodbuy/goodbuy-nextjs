b7fd4ac69a9eecb119b7c74e135f3ac68cb43d00
thatsddr <domenicodiruocco01@gmail.com>
2021-04-02 10:54:34 +0200

    simplified and added types

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -1,21 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { unsetJWTCookie } from "../../lib/apiFunctions/responseHelpers";
+import type { PageConfig } from "next";
 
-export const config = {
+export const config: PageConfig = {
 	api: {
 		bodyParser: false //don't parse the whole request, so we can forward it to the backend
 	}
 };
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default function route(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	return new Promise((resolve, reject): void => {
-		if (req.url === undefined) {
-			res.status(409).json({ message: "error" });
-			reject();
-		} else {
-			unsetJWTCookie(req, res);
-			res.status(200).json({ message: "logged out" });
-			resolve("ok");
-		}
+		unsetJWTCookie(req, res);
+		res.status(200).json({ message: "logged out" });
+		resolve();
 	});
-};
+}
