2a46ff61d4d28366ae2d84de0e7e23837d115fac
thatsddr <domenicodiruocco01@gmail.com>
2021-04-08 10:07:43 +0200

    added test for Field.tsx

---
+++ __tests__/auth/Field.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/jest-dom";
+import { render, act, fireEvent } from "@testing-library/react";
+import Field from "../../components/auth/Field";
+
+describe("Test input field component", () => {
+	let stateValue: string;
+	let isValidValue: boolean;
+	let mockedUpdater = jest.fn().mockImplementation((s: string) => {
+		stateValue = s;
+		isValidValue = s.length > 10 ? true : false;
+	});
+
+	beforeEach(() => {
+		stateValue = "PropertyName";
+		mockedUpdater.mockReset();
+		mockedUpdater = jest.fn().mockImplementation((s) => (stateValue = s));
+	});
+
+	it("Should display an error message", async () => {
+		const { getByPlaceholderText, getByText } = render(
+			<Field value={stateValue} setValue={mockedUpdater} isValidValue={isValidValue} type="text" name="Property" />
+		);
+
+		const field = getByPlaceholderText("Property");
+
+		await act(async () => {
+			fireEvent.change(field, { target: { value: "fail" } });
+		});
+
+		const error = getByText("Invalid Property");
+
+		expect(error).toBeVisible();
+	});
+});
