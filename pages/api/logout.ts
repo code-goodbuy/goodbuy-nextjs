7bc4474b70c104fe3894cb7d508f3d51974e3890
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 10:27:40 +0200

    also unset the refresh token when logging out

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -7,6 +7,7 @@ export const config = apiConfig;
 export default function route(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	return new Promise((resolve, reject): void => {
 		unsetTokenCookie(req, res, "auth-token");
+		unsetTokenCookie(req, res, "refresh-token");
 		res.status(200).json({ message: "logged out" });
 		resolve();
 	});
