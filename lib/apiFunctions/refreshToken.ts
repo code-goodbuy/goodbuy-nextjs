9c976ca8c4a7b8f57d269bc029b77f00490e8135
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 19:17:06 +0200

    fixed name

--- lib/apiFunctions/refreshToken.ts
+++ lib/apiFunctions/refreshToken.ts
@@ -9,7 +9,7 @@ import {
 } from "./commonFunctions";
 import type httpProxy from "http-proxy";
 
-export default function login(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
+export default function refreshToken(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
 	return new Promise((resolve, reject): void => {
 		rejectIfCondition(res, reject, req.url === undefined);
 		const { refreshToken } = getCommonRequirements(req, res);
