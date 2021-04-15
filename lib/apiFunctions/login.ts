b8fa2cb81aa8be249a2290bd26ae7f306b86c30b
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 19:15:09 +0200

    specified handler

--- lib/apiFunctions/login.ts
+++ lib/apiFunctions/login.ts
@@ -1,5 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prepareForForwarding, rejectIfCondition, forwardRequest, handleResponse } from "./commonFunctions";
+import {
+	prepareForForwarding,
+	rejectIfCondition,
+	forwardRequest,
+	handleResponse,
+	handleLogin
+} from "./commonFunctions";
 import type httpProxy from "http-proxy";
 
 export default function login(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
@@ -7,6 +13,6 @@ export default function login(req: NextApiRequest, res: NextApiResponse, proxy: 
 		rejectIfCondition(res, reject, req.url === undefined);
 		prepareForForwarding(req);
 		forwardRequest({ req, res, proxy, handleRes: true, reject });
-		handleResponse({ proxy, resolve, reject });
+		handleResponse({ proxy, resolve, reject, handler: handleLogin });
 	});
 }
