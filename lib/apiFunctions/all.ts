2c4fbfcc80548e9d1c4b78dcedea0d5a5c6f398b
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 18:03:57 +0200

    updated functions names

--- lib/apiFunctions/all.ts
+++ lib/apiFunctions/all.ts
@@ -1,13 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getCommonRequirements, rejectIfInvalid } from "./commonFunctions";
+import { getCommonRequirements, prepareForForwarding, rejectIfCondition } from "./commonFunctions";
 import type httpProxy from "http-proxy";
 
 export default function all(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
 	return new Promise((resolve, reject): void => {
-		rejectIfInvalid(req, res, reject);
+		rejectIfCondition(res, reject, req.url === undefined);
 		const { authToken } = getCommonRequirements(req, res);
-		req.url = req?.url?.replace(/^\/api/, ""); //remove "api" from the url
-		req.headers.cookie = ""; //don't send other cookies to the backend
+		prepareForForwarding(req);
 		if (authToken) {
 			req.headers["auth-token"] = authToken;
 		}
