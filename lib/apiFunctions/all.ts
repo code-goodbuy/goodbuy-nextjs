3381e65772808df793ac4ea3421a7513763bafae
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 19:30:08 +0200

    replaced duplicated code

--- lib/apiFunctions/all.ts
+++ lib/apiFunctions/all.ts
@@ -1,26 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getCommonRequirements, prepareForForwarding, rejectIfCondition } from "./commonFunctions";
+import { forwardRequest, getCommonRequirements, prepareForForwarding, rejectIfCondition } from "./commonFunctions";
 import type httpProxy from "http-proxy";
 
 export default function all(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
 	return new Promise((resolve, reject): void => {
 		rejectIfCondition(res, reject, req.url === undefined);
 		const { authToken } = getCommonRequirements(req, res);
-		prepareForForwarding(req);
-		if (authToken) {
-			req.headers["auth-token"] = authToken;
-		}
-		proxy.web(
-			req,
-			res,
-			{
-				target: process.env.backendURL,
-				autoRewrite: false,
-				selfHandleResponse: false
-			},
-			(e) => {
-				reject(e);
-			}
-		);
+		prepareForForwarding({ req, token: authToken });
+		forwardRequest({ req, res, proxy, handleRes: false, reject });
 	});
 }
