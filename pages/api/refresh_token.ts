347acee121b09fd9845cefdc42f527f88b890321
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 10:27:08 +0200

    new file to handle access token refreshes

---
+++ pages/api/refresh_token.ts
@@ -0,0 +1,53 @@
+import httpProxy from "http-proxy";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { decodeJWT } from "../../lib/apiFunctions/jwtHelpers";
+import { setTokenCookie, getTokenFromResponse, getTokenCookie } from "../../lib/apiFunctions/responseHelpers";
+import { apiConfig } from "../../lib/apiFunctions/apiConfig";
+
+const proxy = httpProxy.createProxyServer({ changeOrigin: true });
+
+export const config = apiConfig;
+
+export default function route(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+	return new Promise((resolve, reject): void => {
+		if (req.url === undefined) {
+			res.status(500).json({ message: "error" });
+			reject();
+		} else {
+			const refreshToken = getTokenCookie(req, res, "refresh-token");
+			req.url = req.url.replace(/^\/api/, ""); //remove "api" from the url
+			req.headers.cookie = `jid=${refreshToken}`;
+
+			proxy.web(
+				req,
+				res,
+				{
+					target: process.env.backendURL,
+					autoRewrite: false,
+					selfHandleResponse: true
+				},
+				(e) => {
+					reject(e);
+				}
+			);
+
+			proxy.once("proxyRes", (proxyRes, req, res: any) => {
+				let apiResponseBody = "";
+				proxyRes.on("data", (chunk: string) => {
+					apiResponseBody += chunk;
+				});
+				proxyRes.on("end", () => {
+					const jwt = getTokenFromResponse(apiResponseBody);
+					if (jwt === null) {
+						res.status(204);
+						reject("Error - No Token");
+					} else {
+						setTokenCookie(req, res, "auth-token", jwt);
+						res.status(200).json({ ...decodeJWT(jwt) });
+						resolve();
+					}
+				});
+			});
+		}
+	});
+}
