df09f4dd4f5a89eb3ebcb2c52982aeaf34c01fea
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 19:15:23 +0200

    new file

---
+++ lib/apiFunctions/refreshToken.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import {
+	prepareForForwarding,
+	rejectIfCondition,
+	forwardRequest,
+	handleResponse,
+	getCommonRequirements,
+	handleRefresh
+} from "./commonFunctions";
+import type httpProxy from "http-proxy";
+
+export default function login(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
+	return new Promise((resolve, reject): void => {
+		rejectIfCondition(res, reject, req.url === undefined);
+		const { refreshToken } = getCommonRequirements(req, res);
+		prepareForForwarding(req, `jid=${refreshToken}`);
+		forwardRequest({ req, res, proxy, handleRes: true, reject });
+		handleResponse({ proxy, resolve, reject, handler: handleRefresh });
+	});
+}
