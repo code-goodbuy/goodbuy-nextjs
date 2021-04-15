7eecc498a61d81c4193fc88978323230b168c6af
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 15:58:49 +0200

    refactored code

--- lib/apiFunctions/check.ts
+++ lib/apiFunctions/check.ts
@@ -1,12 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getCommonRequirements, rejectIfInvalid } from "./commonFunctions";
-import type httpProxy from "http-proxy";
-import { isValidJWT } from "./jwtHelpers";
+import { getCommonRequirements, rejectIfInvalid, resolveReq } from "./commonFunctions";
+import { isExpiredJWT } from "./jwtHelpers";
+import { ResolveIfValidType } from "../types/AuthTypes";
 
-export default function check(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
+function resolveIfValid({ token, response, resolve, message }: ResolveIfValidType) {
+	const isTokenValid = token && isExpiredJWT(token);
+	if (isTokenValid) {
+		return resolveReq(response, resolve, message);
+	}
+}
+
+export default function check(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	return new Promise((resolve, reject): void => {
 		rejectIfInvalid(req, res, reject);
 		const { authToken, refreshToken } = getCommonRequirements(req, res);
-		const isAuthTokenValid = isValidJWT(authToken);
+		resolveIfValid({ token: authToken, response: res, resolve, message: "logged" });
+		resolveIfValid({ token: refreshToken, response: res, resolve, message: "refresh" });
+		return resolveReq(res, resolve, "not logged");
 	});
 }
--- pages/api/check.ts
+++ pages/api/check.ts
@@ -1,47 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getTokenFromCookie, initCookies } from "../../lib/apiFunctions/responseHelpers";
-import { decodeJWT } from "../../lib/apiFunctions/jwtHelpers";
 import { apiConfig } from "../../lib/apiFunctions/apiConfig";
+import check from "../../lib/apiFunctions/check";
 
 export const config = apiConfig;
 
 export default function route(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-	return new Promise((resolve, reject): void => {
-		if (req.url === undefined) {
-			res.status(409).json({ message: "error" });
-			reject();
-		} else {
-			const cookie = initCookies(req, res);
-			const token = getTokenFromCookie(cookie, "auth-token");
-			if (token && token !== "") {
-				let validJWT = false;
-				let data;
-				try {
-					data = decodeJWT(token);
-					validJWT = true;
-				} catch {
-					validJWT = false;
-				}
-				if (validJWT && !(data instanceof Error)) {
-					res.status(200).json({ message: "logged" });
-					resolve();
-				} else {
-					const refreshToken = getTokenFromCookie(cookie, "refresh-token");
-					let validRefresh = false;
-					try {
-						refreshToken && decodeJWT(refreshToken);
-						validRefresh = true;
-					} catch {
-						validRefresh = false;
-					}
-					if (validRefresh) {
-						res.status(200).json({ message: "refresh" });
-						resolve();
-					}
-				}
-			}
-			res.status(200).json({ message: "not logged" });
-			resolve();
-		}
-	});
+	return check(req, res);
 }
