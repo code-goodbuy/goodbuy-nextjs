1e2daf7e13c7aee2916b489436f78938108b1408
thatsddr <domenicodiruocco01@gmail.com>
2021-04-15 18:04:12 +0200

    updated functions names

--- lib/apiFunctions/check.ts
+++ lib/apiFunctions/check.ts
@@ -1,21 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { getCommonRequirements, rejectIfInvalid, resolveReq } from "./commonFunctions";
+import { getCommonRequirements, rejectIfCondition, resolveReq } from "./commonFunctions";
 import { isExpiredJWT } from "./jwtHelpers";
 import { ResolveIfValidType } from "../types/AuthTypes";
 
 function resolveIfValid({ token, response, resolve, message }: ResolveIfValidType) {
-	const isTokenValid = token && isExpiredJWT(token);
+	const isTokenValid = token && !isExpiredJWT(token);
 	if (isTokenValid) {
-		return resolveReq(response, resolve, message);
+		return resolveReq(response, resolve, { "message": message });
 	}
 }
 
 export default function check(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	return new Promise((resolve, reject): void => {
-		rejectIfInvalid(req, res, reject);
+		rejectIfCondition(res, reject, req.url === undefined);
 		const { authToken, refreshToken } = getCommonRequirements(req, res);
 		resolveIfValid({ token: authToken, response: res, resolve, message: "logged" });
 		resolveIfValid({ token: refreshToken, response: res, resolve, message: "refresh" });
-		return resolveReq(res, resolve, "not logged");
+		return resolveReq(res, resolve, { "message": "not logged" });
 	});
 }
