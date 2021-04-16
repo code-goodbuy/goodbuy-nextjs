0606cf7ed8f5edbcf6fce7ddce00b73f7b4e933e
thatsddr <domenicodiruocco01@gmail.com>
2021-04-16 12:35:34 +0200

    added new tests

--- __tests__/api/commonFunctions.test.ts
+++ __tests__/api/commonFunctions.test.ts
@@ -1,9 +1,16 @@
-import { rejectIfCondition, resolveIfValid, prepareForForwarding } from "../../lib/apiFunctions/commonFunctions";
+import {
+	rejectIfCondition,
+	resolveIfValid,
+	prepareForForwarding,
+	forwardRequest,
+	handleLogin,
+	resolveReq
+} from "../../lib/apiFunctions/commonFunctions";
 import mock from "mock-http";
-import { IncomingMessage } from "node:http";
+import { NextApiRequest } from "next";
 
 describe("Test the functions that make the proxy work", () => {
-	let req: IncomingMessage, res: any, token: string;
+	let req: NextApiRequest, res: any, rej: any, resolve: any, token: string;
 
 	let mockGetCookie = jest.fn((name) => name);
 	let mockSetCookie = jest.fn();
@@ -16,32 +23,52 @@ describe("Test the functions that make the proxy work", () => {
 	});
 
 	beforeEach(() => {
-		req = new mock.Request({
+		token =
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE5OTk5OTk5OTksImVtYWlsIjoidGVzdEB0ZXN0LmNvIiwiaWF0IjoxNTE2MjM5MDIyfQ.JyrM7CQymsHChoqFcj_-VCJwn0mDQtN9r8jnEJ_TySw";
+
+		const temp = new mock.Request({
 			url: "/test",
 			method: "POST",
 			buffer: Buffer.from("name=mock&version=first")
 		});
+		req = Object.assign(temp, { "cookies": {}, "query": {}, "body": {}, "env": {} });
 		res = new mock.Response({});
 		res.status = jest.fn(() => ({ "json": jest.fn() }));
+		rej = jest.fn();
+		resolve = jest.fn();
 	});
 
 	it("should reject the request", () => {
-		const rej = jest.fn();
 		// @ts-ignore: res doens't match the exact type but has all the required properties
 		rejectIfCondition(res, rej, true);
 		expect(rej).toHaveBeenCalled();
 	});
 
 	it("should not resolve the request", () => {
-		const resolve = jest.fn();
 		resolveIfValid({ token: "invalidToken", response: res, resolve, message: "test" });
 		expect(resolve).not.toHaveBeenCalled();
 	});
 
 	it("should modify the request", () => {
-		const temp = Object.assign(req, { "cookies": {}, "query": {}, "body": {}, "env": {} });
-		prepareForForwarding({ req: temp, cookie: "test-cookie", token: "test-token" });
-		expect(temp.headers.cookie).toEqual("test-cookie");
-		expect(temp.headers["auth-token"]).toEqual("test-token");
+		prepareForForwarding({ req, cookie: "test-cookie", token: "test-token" });
+		expect(req.headers.cookie).toEqual("test-cookie");
+		expect(req.headers["auth-token"]).toEqual("test-token");
+	});
+
+	it("should forward a request", () => {
+		const proxy = { web: jest.fn() };
+		// @ts-ignore; proxy type is not exactly the same
+		forwardRequest({ res, res, proxy, handleRes: false, reject: rej });
+		expect(proxy.web).toHaveBeenCalled();
+		expect(rej).not.toHaveBeenCalled();
+	});
+
+	it("should handle the login", () => {
+		res._internal.headers = { "set-cookie": `jid=${token}; other things;` };
+		res._internal.body = `{"jwtAccessToken":"${token}"}`;
+		// @ts-ignore;
+		handleLogin({ req, res, proxyRes: res._internal, body: `{"jwtAccessToken":"${token}"}`, resolve, reject: rej });
+		expect(resolve).toHaveBeenCalled();
+		expect(rej).not.toHaveBeenCalled();
 	});
 });
