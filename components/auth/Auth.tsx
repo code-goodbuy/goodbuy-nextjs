498ce1bf1bb4e26afa35a81a6683f8bfd8898171
thatsddr <domenicodiruocco01@gmail.com>
2021-04-17 11:08:43 +0200

    refactored setAction

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -18,33 +18,28 @@ export default function Auth() {
 		};
 	}, [isAuthenticating]);
 
+	function changeAction(text: string, newState: "login" | "sign-up") {
+		return (
+			<div
+				className={"action-selector rounded-tl-md" + (action !== newState && " colorful-bg-2")}
+				onClick={() => {
+					setAction(newState);
+				}}
+			>
+				{text}
+			</div>
+		);
+	}
+
 	return (
 		<div className="min-h-full normal-bg flex flex-col justify-center items-center pt-14 md:pt-10 pb-5">
 			<div className="rounded-md ml-8 mr-8 normal-bg ring-2 ring-primary dark:ring-secondary w-90 lg:w-50% flex flex-col mt-4 md:mt-12">
 				<div className="flex flex-row">
-					<div
-						className={"action-selector rounded-tl-md" + (action === "sign-up" && " colorful-bg-2")}
-						onClick={() => {
-							setAction("login");
-						}}
-					>
-						Login
-					</div>
-					<div
-						className={"action-selector rounded-tr-md" + (action === "login" && " colorful-bg-2")}
-						onClick={() => {
-							setAction("sign-up");
-						}}
-					>
-						Sign Up
-					</div>
+					{changeAction("Login", "login")}
+					{changeAction("Sign Up", "sign-up")}
 				</div>
 				<div>
-					{action === "login" ? (
-						<LoginForm />
-					) : (
-						<SignUpForm setAction={setAction} msBeforeRedirecting={1000} />
-					)}
+					{action === "login" ? <LoginForm /> : <SignUpForm setAction={setAction} msBeforeRedirecting={1000} />}
 				</div>
 			</div>
 		</div>
