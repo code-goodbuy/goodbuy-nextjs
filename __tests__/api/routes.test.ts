2a4aa39c3c64ec98bbcf5d1c8dc8bb393cf2bbaf
thatsddr <domenicodiruocco01@gmail.com>
2021-04-17 09:42:30 +0200

    renamed

--- __tests__/api/routes.test.ts
+++ __tests__/api/routes.test.ts
@@ -2,7 +2,6 @@ import "@testing-library/jest-dom";
 import { testApiHandler } from "next-test-api-route-handler";
 import route, { config } from "../../pages/api/logout";
 import type { PageConfig } from "next";
-import mock from "mock-http";
 
 const handler: typeof route & { config?: PageConfig } = route;
 handler.config = config;
@@ -10,10 +9,17 @@ handler.config = config;
 describe("test log out route", () => {
 	it("should log out the user", async () => {
 		await testApiHandler({
+			requestPatcher: (req) => {
+				req.url = "/api/logout";
+			},
 			handler,
-			requestPatcher: (req) => new mock.Request(),
 			test: async ({ fetch }) => {
-				const res = await fetch({ method: "POST" });
+				const res = await fetch({
+					method: "POST",
+					headers: {
+						"content-type": "application/json"
+					}
+				});
 				const data = await res.json();
 				expect(data).toStrictEqual({ message: "logged out" });
 			}
