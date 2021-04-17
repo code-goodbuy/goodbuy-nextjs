844ab91a44b276cfb08a9c652e832ad6da8cdd18
thatsddr <domenicodiruocco01@gmail.com>
2021-04-17 11:23:41 +0200

    updated tests

--- __tests__/auth/SignUpLogic.test.tsx
+++ __tests__/auth/SignUpLogic.test.tsx
@@ -54,7 +54,7 @@ describe("test sign up logic", () => {
 		fireEvent.click(getByText("Sign Up"));
 
 		await waitFor(async () =>
-			expect(global.fetch).toHaveBeenCalledWith("http://localhost/api/register", {
+			expect(global.fetch).toHaveBeenCalledWith("/api/register", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json"
