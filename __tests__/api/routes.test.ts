beec22b5e3ff3952e295d98e67394274e5af831c
thatsddr <domenicodiruocco01@gmail.com>
2021-04-17 10:27:16 +0200

    also test teh check route

--- __tests__/api/routes.test.ts
+++ __tests__/api/routes.test.ts
@@ -1,28 +1,47 @@
 import "@testing-library/jest-dom";
 import { testApiHandler } from "next-test-api-route-handler";
-import route, { config } from "../../pages/api/logout";
+import logout from "../../pages/api/logout";
+import check from "../../pages/api/check";
+import { apiConfig as config } from "../../lib/apiFunctions/apiConfig";
 import type { PageConfig } from "next";
 
-const handler: typeof route & { config?: PageConfig } = route;
+let handler: typeof logout & { config?: PageConfig } = logout;
 handler.config = config;
 
-describe("test log out route", () => {
+async function testRoute(route: any, url: string, tests: any) {
+	handler = route;
+	handler.config = config;
+
+	await testApiHandler({
+		requestPatcher: (req) => {
+			req.url = url;
+		},
+		handler,
+		test: async ({ fetch }) => {
+			const res = await fetch({
+				method: "POST",
+				headers: {
+					"content-type": "application/json"
+				}
+			});
+			const data = await res.json();
+			tests(data);
+		}
+	});
+}
+
+describe("test routes", () => {
 	it("should log out the user", async () => {
-		await testApiHandler({
-			requestPatcher: (req) => {
-				req.url = "/api/logout";
-			},
-			handler,
-			test: async ({ fetch }) => {
-				const res = await fetch({
-					method: "POST",
-					headers: {
-						"content-type": "application/json"
-					}
-				});
-				const data = await res.json();
-				expect(data).toStrictEqual({ message: "logged out" });
-			}
-		});
+		const tests = (data: any) => {
+			expect(data).toStrictEqual({ message: "logged out" });
+		};
+		await testRoute(logout, "/api/logout", tests);
+	});
+
+	it("should check the user logged in state", async () => {
+		const tests = (data: any) => {
+			expect(data).toStrictEqual({ message: "not logged" });
+		};
+		await testRoute(check, "/api/check", tests);
 	});
 });
