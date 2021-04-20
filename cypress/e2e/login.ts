f27bccce8f41f1b8080ac844c977eb607e9cc8fe
thatsddr <domenicodiruocco01@gmail.com>
2021-04-20 13:27:37 +0200

    use cypress testing library to get teh fields

--- cypress/e2e/login.ts
+++ cypress/e2e/login.ts
@@ -1,11 +1,12 @@
+/// <reference types="@testing-library/cypress" />
 /// <reference types="cypress" />
 
 describe("Test login", () => {
 	it("Should log in a user", () => {
 		cy.visit("/auth");
-		cy.get('[type="text"]').click().type("tester2@mail.co");
-		cy.get('[type="password"]').click().type("Password1!");
-		cy.get("#login-form > .colorful-button").click();
+		cy.findByPlaceholderText("Email").click().type("tester2@mail.co");
+		cy.findByPlaceholderText("Password").click().type("Password1!");
+		cy.findByText("Log In").click();
 		cy.get(".space-x-4 > .colorful-button").should("have.text", "Log Out");
 	});
 });
