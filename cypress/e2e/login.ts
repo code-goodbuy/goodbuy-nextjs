4f87b46943c2e0d4e796f9ee775b8bfb9620f450
thatsddr <domenicodiruocco01@gmail.com>
2021-04-21 16:05:05 +0200

    updated tester email

--- cypress/e2e/login.ts
+++ cypress/e2e/login.ts
@@ -4,7 +4,7 @@
 describe("Test login", () => {
 	it("Should log in a user", () => {
 		cy.visit("/auth");
-		cy.findAllAndType({ "Email": "tester2@mail.co", "Password": "Password1!" });
+		cy.findAllAndType({ "Email": "goodbuytester@cuvox.de", "Password": "Password1!" });
 		cy.findByText("Log In").click();
 		cy.url().should("eq", Cypress.config().baseUrl + "/");
 		cy.get(".space-x-4 > .colorful-button").should("have.text", "Log Out");
@@ -12,7 +12,7 @@ describe("Test login", () => {
 
 	it("Should display an error", () => {
 		cy.visit("/auth");
-		cy.findAllAndType({ "Email": "tester2@mail.co", "Password": "not a password" });
+		cy.findAllAndType({ "Email": "goodbuytester@cuvox.de", "Password": "not a password" });
 		cy.findByText("Log In").click();
 		cy.url().should("eq", Cypress.config().baseUrl + "/auth");
 		cy.findByText("An Error Occured");
