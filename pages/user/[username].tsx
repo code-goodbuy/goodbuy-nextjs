fc2089dc8dd925ffea53f04fc8a1d98c3d4b973d
thatsddr <domenicodiruocco01@gmail.com>
2021-04-21 14:36:25 +0200

    added meta tag

--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -1,3 +1,5 @@
+import Meta from "../../components/common/Meta";
+
 function User({ username, data }: { username: string; data: any }) {
 	if (data.message === "Not Found") {
 		return (
@@ -7,12 +9,15 @@ function User({ username, data }: { username: string; data: any }) {
 		);
 	}
 	return (
-		<div className="min-h-screen normal-bg">
-			<p className="pt-40 colorful-text text-2xl font-bold m-auto">{username}</p>
-			<p className="normal-text">Scanned: {data?.scannedProducts || 0}</p>
-			<p className="normal-text">Followers: {data?.followers || 0}</p>
-			<p className="normal-text">Following: {data?.following || 0}</p>
-		</div>
+		<>
+			<Meta title={`${username}'s Profile | Goodbuy`}></Meta>
+			<div className="min-h-screen normal-bg">
+				<p className="pt-40 colorful-text text-2xl font-bold m-auto">{username}</p>
+				<p className="normal-text">Scanned: {data?.scannedProducts || 0}</p>
+				<p className="normal-text">Followers: {data?.followers || 0}</p>
+				<p className="normal-text">Following: {data?.following || 0}</p>
+			</div>
+		</>
 	);
 }
 
