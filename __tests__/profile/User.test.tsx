d2db4869d21e59c3e14403accc9f740a378f9b7b
thatsddr <domenicodiruocco01@gmail.com>
2021-04-21 13:15:25 +0200

    TDD: added code to make test pass

--- __tests__/profile/User.test.tsx
+++ __tests__/profile/User.test.tsx
@@ -4,7 +4,19 @@ import User from "../../pages/user/[username]";
 
 describe("Test profile page", () => {
 	it("Should render the informations properly", () => {
-		const { getByText } = render(<User username="test"></User>);
+		const { getByText } = render(
+			<User
+				username="test"
+				data={{
+					followers: "50",
+					following: "70",
+					scannedProducts: "30",
+					userID: "mockUserId",
+					description: "I like using goodbuy",
+					imageURL: "/public/pics/face.png"
+				}}
+			></User>
+		);
 
 		const username = getByText("test");
 		const scanned = getByText("Scanned: 30");
--- pages/user/[username].tsx
+++ pages/user/[username].tsx
@@ -1,13 +1,18 @@
-function User({ username }: { username: string }) {
+function User({ username, data }: { username: string; data: any }) {
 	return (
 		<div className="min-h-screen normal-bg">
-			<p className="pt-40 colorful-text text-6xl font-bold m-auto">{username}</p>
+			<p className="pt-40 colorful-text text-2xl font-bold m-auto">{username}</p>
+			<p className="normal-text">Scanned: {data?.scannedProducts || 0}</p>
+			<p className="normal-text">Followers: {data?.followers || 0}</p>
+			<p className="normal-text">Following: {data?.following || 0}</p>
 		</div>
 	);
 }
 
 export async function getServerSideProps({ query }: { query: any }) {
-	return { props: { username: query.username } };
+	const res = await fetch("http://localhost:3000/api/dev/get-info");
+	const data = await res.json();
+	return { props: { username: query.username, data } };
 }
 
 export default User;
