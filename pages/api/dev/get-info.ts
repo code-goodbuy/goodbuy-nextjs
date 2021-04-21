32a924e987b605bdf5e61604d9e0adad0733a78e
thatsddr <domenicodiruocco01@gmail.com>
2021-04-21 13:32:57 +0200

    mock not found when the username is shorter than 6 characters

--- pages/api/dev/get-info.ts
+++ pages/api/dev/get-info.ts
@@ -2,10 +2,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function route(req: NextApiRequest, res: NextApiResponse): Promise<void> {
 	return new Promise((resolve, reject) => {
+		if (req.body.user.length < 6) {
+			resolve();
+			res.status(404).send({ "message": "user does not exist" });
+		}
 		const data = {
 			followers: "50",
 			following: "70",
-			numberOfScannedProducts: "30",
+			scannedProducts: "30",
 			userID: "mockUserId",
 			description: "I like using goodbuy",
 			imageURL: "/public/pics/face.png"
