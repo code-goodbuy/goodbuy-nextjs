dde84ed36e63643a784e3d3cec7065f61e621d0b
thatsddr <domenicodiruocco01@gmail.com>
2021-04-22 10:52:11 +0200

    border color now managed by global style

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -2,7 +2,7 @@ import { PostType } from "../../lib/types/PostTypes";
 
 export default function Post({ title, EAN, country, username }: PostType) {
 	return (
-		<div className="border-2 rounded-2xl m-4 p-2 border-primary dark:border-secondary normal-text w-90 lg:w-50% max-w-3xl">
+		<div className="colorful-border rounded-2xl m-4 p-2 normal-text w-90 lg:w-50% max-w-3xl">
 			<p>{username}</p>
 			<h3 className="colorful-text">{title}</h3>
 			<p>EAN: {EAN}</p>
