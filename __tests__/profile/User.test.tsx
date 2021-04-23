9be960620bfc0061deef24a5e30b123da78db894
thatsddr <domenicodiruocco01@gmail.com>
2021-04-23 14:30:26 +0200

    mock useRouter

--- __tests__/profile/User.test.tsx
+++ __tests__/profile/User.test.tsx
@@ -8,6 +8,28 @@ function expectAllToBeVisible(elements: HTMLElement[]) {
 	}
 }
 
+const useRouter = jest.spyOn(require("next/router"), "useRouter");
+
+useRouter.mockImplementation(() => ({
+	route: "",
+	basePath: "",
+	pathname: "/",
+	query: {},
+	asPath: "",
+	push: async () => true,
+	replace: async () => true,
+	reload: () => null,
+	back: () => null,
+	prefetch: async () => undefined,
+	beforePopState: () => null,
+	isFallback: false,
+	events: {
+		on: () => null,
+		off: () => null,
+		emit: () => null
+	}
+}));
+
 describe("Test profile page", () => {
 	it("Should render the informations properly", () => {
 		// given + when
