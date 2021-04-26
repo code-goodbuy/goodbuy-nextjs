90017a0f52137d13488985abaaeacd033083ad3a
thatsddr <domenicodiruocco01@gmail.com>
2021-04-26 16:04:37 +0200

    PWA

--- components/common/Meta.tsx
+++ components/common/Meta.tsx
@@ -13,6 +13,7 @@ const Meta = ({ title, keywords, description }: MetaType) => {
 			<meta name="theme-color" content={colorMode === "dark" ? "#000000" : "#ffffff"} />
 			<meta charSet="utf-8" />
 			<link rel="icon" href="/icons/favicon.ico" />
+			<link rel="manifest" href="/manifest.json" />
 			<link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
 			<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
 			<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
