b80b2d5d341da01cea3c7ab7a8aa5188ade5406d
thatsddr <domenicodiruocco01@gmail.com>
2021-04-27 15:35:21 +0200

    use Home component when the user is logged in

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,21 +2,15 @@ import { useContext } from "react";
 import { AuthContext } from "../lib/context/AuthContext";
 import Meta from "../components/common/Meta";
 import LandingPage from "../components/landingPage/LandingPage";
-import Feed from "../components/home/Feed";
+import Home from "../components/home/Home";
 
-export default function Home() {
-	/**
-	 * Renders The Landing Page
-	 */
-	const { isLoggedIn, userInfo } = useContext(AuthContext);
+export default function App() {
+	const { isLoggedIn } = useContext(AuthContext);
 	if (isLoggedIn === true) {
 		return (
 			<>
 				<Meta title={"GoodBuy | You are logged in"} />
-				<div className="h-auto normal-bg">
-					<p className="pt-20 pl-10 normal-text">email: {userInfo?.email}</p>
-				</div>
-				<Feed />
+				<Home />
 			</>
 		);
 	} else if (isLoggedIn === false) {
