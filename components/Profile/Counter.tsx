6925a36f8abf2a592169e34f034dbb63c202a61a
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 12:02:12 +0200

    specified text color

--- components/Profile/Counter.tsx
+++ components/Profile/Counter.tsx
@@ -1,6 +1,6 @@
 export default function Counter({ number, name }: { number: number; name: string }) {
 	return (
-		<div className="text-center p-4">
+		<div className="text-center normal-text p-4">
 			<h3 className="text-lg font-bold">{number || 0}</h3>
 			<p>{name}</p>
 		</div>
