7f91ddfb9d4e3a9a8e694ae6d66c5148e904234f
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 11:00:38 +0200

    refactored profile image

--- components/common/Post.tsx
+++ components/common/Post.tsx
@@ -1,14 +1,12 @@
 import { PostType } from "../../lib/types/PostTypes";
 import Link from "next/link";
+import { ProfilePic } from "./Misc";
 
 export default function Post({ title, EAN, country, likes, username, imageURL }: PostType) {
 	return (
 		<div className="border-2 colorful-border rounded-2xl my-6 mx-auto p-2 normal-text w-90 lg:w-11/12 max-w-3xl shadow-lg">
 			<div className="flex flex-row items-center">
-				<div
-					className="rounded-full border-4 colorful-border square-50 img-bg"
-					style={{ backgroundImage: "url(" + imageURL + ")" }}
-				></div>
+				<ProfilePic imageURL={imageURL} />
 				<p className="pl-2">
 					<Link href={"/user/" + username}>
 						<a>{username}</a>
