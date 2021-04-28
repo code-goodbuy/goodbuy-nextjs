09d47bde756b3a2709ac9bd43cf7fe560272e57f
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 10:32:42 +0200

    fixed graphical bug

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -3,8 +3,9 @@ import Search from "./Search";
 
 export default function Home() {
 	return (
-		<div className="flex flex-col items-center normal-bg w-full lg:flex-row lg:items-start lg: justify-evenly">
+		<div className="flex flex-col min-h-full items-center normal-bg w-full lg:flex-row lg:items-start justify-evenly">
 			<Search />
+
 			<Feed />
 		</div>
 	);
