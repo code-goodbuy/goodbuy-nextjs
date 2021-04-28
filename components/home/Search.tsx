274c460707a4d3cf9c0bfc63e5ef79ab71dd506f
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 10:11:03 +0200

    search component is now a form

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -10,7 +10,13 @@ export default function Search() {
 	};
 
 	return (
-		<div className="normal-bg mt-28 w-75 lg:w-3/12 lg:max-w-3xl m-2 flex justify-center ">
+		<form
+			className="normal-bg mt-28 w-75 lg:w-3/12 lg:max-w-3xl m-2 flex justify-center"
+			onSubmit={(e) => {
+				e.preventDefault();
+				query.length > 4 && router.push("/user/" + query);
+			}}
+		>
 			<input
 				type="text"
 				placeholder="Search User"
@@ -18,14 +24,11 @@ export default function Search() {
 				value={query}
 				className="outline-none border-0 normal-text text-lg field-bg w-4/5 rounded-tl-full rounded-bl-full focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
 			/>
-			<button
-				onClick={() => {
-					query.length > 4 && router.push("/user/" + query);
-				}}
+			<input
+				type="submit"
+				value="🔍"
 				className="colorful-bg pl-2 pr-4 rounded-tr-full rounded-br-full hover:bg-gray-200 dark:hover:bg-gray-700"
-			>
-				🔍
-			</button>
-		</div>
+			/>
+		</form>
 	);
 }
