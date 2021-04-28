98d2f0c5ce67f16daf21618d5871a690fdae5395
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 11:57:36 +0200

    added title and improved

--- components/home/Suggestions.tsx
+++ components/home/Suggestions.tsx
@@ -6,7 +6,7 @@ export default function Suggestions() {
 	const [suggestions, setSuggestions] = useState<SuggestionType[] | undefined>(undefined);
 
 	async function getSuggestions() {
-		let res = await fetch("/api/dev/get-feed");
+		let res = await fetch("/api/dev/get-suggestions");
 		let data = await res.json();
 		setSuggestions(data.suggestions);
 	}
@@ -16,9 +16,10 @@ export default function Suggestions() {
 	}, []);
 
 	return (
-		<div className="lg:flex lg:flex-row sm:hidden">
+		<div className="lg:flex lg:flex-col sm:hidden lg:fixed lg:top-56 lg:left-8 lg:w-5/12 lg:max-w-3xl mb-2 suggestions-height overflow-y-auto">
+			<h1 className="colorful-text font-bold text-2xl mx-auto text-center">Suggestions</h1>
 			{suggestions?.map((s) => (
-				<Suggestion {...s} />
+				<Suggestion key={s.username} {...s} />
 			))}
 		</div>
 	);
