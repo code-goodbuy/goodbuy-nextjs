e584a277a84377ba41ad5bd77e52a170249f110a
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 10:32:59 +0200

    search field now doesn't move on larger screens

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -10,25 +10,27 @@ export default function Search() {
 	};
 
 	return (
-		<form
-			className="normal-bg mt-28 w-75 lg:w-3/12 lg:max-w-3xl m-2 flex justify-center"
-			onSubmit={(e) => {
-				e.preventDefault();
-				query.length > 4 && router.push("/user/" + query);
-			}}
-		>
-			<input
-				type="text"
-				placeholder="Search User"
-				onChange={(e) => handleChange(e.target.value)}
-				value={query}
-				className="outline-none border-0 normal-text text-lg field-bg w-4/5 rounded-tl-full rounded-bl-full focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
-			/>
-			<input
-				type="submit"
-				value="🔍"
-				className="colorful-bg pl-2 pr-4 rounded-tr-full rounded-br-full hover:bg-gray-200 dark:hover:bg-gray-700"
-			/>
-		</form>
+		<div className="w-75 lg:w-5/12 lg:max-w-3xl">
+			<form
+				className="normal-bg lg:w-5/12 mt-28 m-2 flex justify-center lg:fixed lg:top-8 lg:left-8"
+				onSubmit={(e) => {
+					e.preventDefault();
+					query.length > 4 && router.push("/user/" + query);
+				}}
+			>
+				<input
+					type="text"
+					placeholder="Search User"
+					onChange={(e) => handleChange(e.target.value)}
+					value={query}
+					className="outline-none border-0 normal-text text-lg field-bg w-4/5 rounded-tl-full rounded-bl-full focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
+				/>
+				<input
+					type="submit"
+					value="🔍"
+					className="colorful-bg pl-2 pr-4 rounded-tr-full rounded-br-full hover:bg-gray-200 dark:hover:bg-gray-700"
+				/>
+			</form>
+		</div>
 	);
 }
