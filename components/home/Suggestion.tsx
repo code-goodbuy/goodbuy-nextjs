05c4f879216932e3db35ae071c2d0d27c59e65af
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 11:57:12 +0200

    improved suggestion component

--- components/home/Suggestion.tsx
+++ components/home/Suggestion.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import { SuggestionType } from "../../lib/types/SuggestionTypes";
 import { ProfilePic } from "../common/Misc";
 
 export default function Suggestion({ scannedProducts, imageURL, username }: SuggestionType) {
 	return (
-		<div className="flex flex-row">
+		<div className="flex flex-row items-center my-4 w-80 mx-auto">
 			<ProfilePic imageURL={imageURL} />
-			<p className="colorful-text">{username}</p>
-			<p className="normal-text">{scannedProducts}</p>
+			<Link href={"/user/" + username}>
+				<a className="colorful-text font-bold text-lg pl-4">{username}</a>
+			</Link>
+			<p className="normal-text pl-4">{scannedProducts} Products</p>
 		</div>
 	);
 }
