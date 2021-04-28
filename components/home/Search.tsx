98bf8f7464bc4364f41557df1ba10a6dc8ada6a5
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 11:56:42 +0200

    removed container div

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -10,27 +10,25 @@ export default function Search() {
 	};
 
 	return (
-		<div className="w-75 lg:w-5/12 lg:max-w-3xl sticky top-7">
-			<form
-				className="normal-bg lg:w-5/12 lg:max-w-3xl mt-28 m-2 flex justify-center lg:fixed lg:top-8 lg:left-8"
-				onSubmit={(e) => {
-					e.preventDefault();
-					query.length > 4 && router.push("/user/" + query);
-				}}
-			>
-				<input
-					type="text"
-					placeholder="Search User"
-					onChange={(e) => handleChange(e.target.value)}
-					value={query}
-					className="outline-none border-0 normal-text text-lg field-bg w-4/5 rounded-tl-full rounded-bl-full focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
-				/>
-				<input
-					type="submit"
-					value="🔍"
-					className="colorful-bg pl-2 pr-4 rounded-tr-full rounded-br-full hover:bg-gray-200 dark:hover:bg-gray-700"
-				/>
-			</form>
-		</div>
+		<form
+			className="normal-bg lg:w-5/12 lg:max-w-3xl mt-28 m-2 flex justify-center lg:fixed lg:top-4 lg:left-8"
+			onSubmit={(e) => {
+				e.preventDefault();
+				query.length > 4 && router.push("/user/" + query);
+			}}
+		>
+			<input
+				type="text"
+				placeholder="Search User"
+				onChange={(e) => handleChange(e.target.value)}
+				value={query}
+				className="outline-none border-0 normal-text text-lg field-bg w-4/5 rounded-tl-full rounded-bl-full focus:ring-2 focus:ring-primary dark:focus:ring-2 dark:focus:ring-secondary"
+			/>
+			<input
+				type="submit"
+				value="🔍"
+				className="colorful-bg pl-2 pr-4 rounded-tr-full rounded-br-full hover:bg-gray-200 dark:hover:bg-gray-700"
+			/>
+		</form>
 	);
 }
