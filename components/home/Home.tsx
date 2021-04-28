e334c1b16601851e8a109c84aebfb26e487460d8
thatsddr <domenicodiruocco01@gmail.com>
2021-04-28 11:56:25 +0200

    added suggestions

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -1,10 +1,14 @@
 import Feed from "./Feed";
 import Search from "./Search";
+import Suggestions from "./Suggestions";
 
 export default function Home() {
 	return (
 		<div className="flex flex-col min-h-full items-center normal-bg w-full lg:flex-row lg:items-start justify-evenly">
-			<Search />
+			<div className="w-75 lg:w-5/12 lg:max-w-3xl">
+				<Search />
+				<Suggestions />
+			</div>
 
 			<Feed />
 		</div>
