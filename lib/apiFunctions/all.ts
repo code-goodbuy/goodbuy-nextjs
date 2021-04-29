76719643bf2e2fd0b8e8c895da56b6b767460773
thatsddr <domenicodiruocco01@gmail.com>
2021-04-29 11:54:51 +0200

    added jid to requests to forward

--- lib/apiFunctions/all.ts
+++ lib/apiFunctions/all.ts
@@ -5,8 +5,8 @@ import { initCookies } from "./responseHelpers";
 
 export default function all(req: NextApiRequest, res: NextApiResponse, proxy: httpProxy): Promise<void> {
 	return new Promise((resolve, reject): void => {
-		const { authToken } = getCommonRequirements(initCookies(req, res));
-		prepareForForwarding({ req, token: authToken });
+		const { authToken, refreshToken } = getCommonRequirements(initCookies(req, res));
+		prepareForForwarding({ req, token: authToken, cookie: `jid=${refreshToken}` });
 		forwardRequest({ req, res, proxy, handleRes: false, reject });
 	});
 }
