beefd14b9134812b8098ee7be6221b5cb0e77aa9
thatsddr <domenicodiruocco01@gmail.com>
2021-04-29 15:58:03 +0200

    fixed graphical bug

--- components/home/Suggestions.tsx
+++ components/home/Suggestions.tsx
@@ -16,7 +16,7 @@ export default function Suggestions() {
 	}, []);
 
 	return (
-		<div className="lg:flex lg:flex-col sm:hidden lg:fixed lg:top-56 lg:left-8 lg:w-5/12 lg:max-w-3xl mb-2 suggestions-height overflow-y-auto">
+		<div className="lg:flex lg:flex-col hidden lg:fixed lg:top-56 lg:left-8 lg:w-5/12 lg:max-w-3xl mb-2 suggestions-height overflow-y-auto">
 			<h1 className="colorful-text font-bold text-2xl mx-auto text-center">Suggestions</h1>
 			{suggestions?.map((s) => (
 				<Suggestion key={s.username} {...s} />
