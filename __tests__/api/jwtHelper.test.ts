2d1500bcda95481f262053e90bf21f751576fff4
thatsddr <domenicodiruocco01@gmail.com>
2021-05-03 15:09:33 +0200

    improved the descriptions

--- __tests__/api/jwtHelper.test.ts
+++ __tests__/api/jwtHelper.test.ts
@@ -1,7 +1,7 @@
 import { decodeJWT, isValidJWT } from "../../lib/apiFunctions/jwtHelpers";
 
 describe("test API logic", () => {
-	it("should return an object", () => {
+	it("should return an object from a valid token", () => {
 		// given + when
 		const res = decodeJWT(
 			"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InRlc3RAdGVzdC50ZXN0IiwiaWF0IjoxNjE2NzUwOTA3LCJleHAiOjQwMDAwMDAwMDAsImp0aSI6IjZlMDA3MTJmLTIxOTYtNGJjYS1iMzhmLTQ4MTFkOGI5MTAxOSJ9.hHvO7GQk4cqU1fL8-Ec6u7IFfpmBxMfNGsLKUqLeS64"
@@ -15,14 +15,14 @@ describe("test API logic", () => {
 		});
 	});
 
-	it("should return false", () => {
+	it("should return false from an invalid token", () => {
 		// given + when
 		const res = isValidJWT("notAJwtToken");
 		//then
 		expect(res).toBe(false);
 	});
 
-	it("shouhld throw an error", () => {
+	it("shouhld throw an error from an invalid token", () => {
 		//given
 		let res = "";
 		//when
