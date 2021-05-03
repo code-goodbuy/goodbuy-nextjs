65a22574f6d4661aa9f6c56c64e2699d3e88b016
thatsddr <domenicodiruocco01@gmail.com>
2021-05-03 15:57:08 +0200

    removed duplication and automatically formatted

--- __tests__/utility/Scanner.test.tsx
+++ __tests__/utility/Scanner.test.tsx
@@ -2,63 +2,63 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ScannerPage from "../../components/utility/ScannerPage";
 // Mock Service Worker
-import { rest } from 'msw'
-import { setupServer } from 'msw/node'
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 // @ts-ignore
-import { FetchMock } from '@react-mock/fetch';
+import { FetchMock } from "@react-mock/fetch";
+import { clickAll } from "../../lib/testUtils/testFunctions";
 
 // need self signed TLS Certs for reading camera list
 // currently test being skipped cause there's no element in the page yet
 
 const server = setupServer(
-  rest.get('/greeting', (req, res, ctx) => {
-    return res(ctx.json({ greeting: 'hello there' }))
-  })
-)
+	rest.get("/greeting", (req, res, ctx) => {
+		return res(ctx.json({ greeting: "hello there" }));
+	})
+);
 
 beforeAll(() => {
-  server.listen()
-  // silence error log
-  jest.spyOn(console, 'log').mockImplementation(jest.fn());
-  jest.spyOn(console, 'debug').mockImplementation(jest.fn());
-})
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
+	server.listen();
+	// silence error log
+	jest.spyOn(console, "log").mockImplementation(jest.fn());
+	jest.spyOn(console, "debug").mockImplementation(jest.fn());
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe("Scanner Component", () => {
-  it("open modal and should be able to read the title", () => {
-    // given: arrange
-    render(<ScannerPage />);
-    // when: act
-    fireEvent.click(screen.getByText('Scanner'));
-    // then: assert
-    expect(screen.getByText('Barcode')).toBeVisible();
-  });
+	it("open modal and should be able to read the title", () => {
+		// given: arrange
+		render(<ScannerPage />);
+		// when: act
+		fireEvent.click(screen.getByText("Scanner"));
+		// then: assert
+		expect(screen.getByText("Barcode")).toBeVisible();
+	});
 
-  it("find start button and check the scanner element", () => {
-    render(<ScannerPage />);
-    fireEvent.click(screen.getByText('Scanner'));
-    fireEvent.click(screen.getByText('Start'));
-    expect(screen.queryByTestId('video-elm')).toBeVisible();
-  });
+	it("find start button and check the scanner element", () => {
+		render(<ScannerPage />);
 
-  it("send the result and receives the product info", async () => {
-    const alertMock = jest.spyOn(global, 'alert');
-    render(
-      <FetchMock
-        matcher="/product/123456"
-        response={200}
-        config={{ fallbackToNetwork: true }}
-      >
-        <ScannerPage />
-      </FetchMock>);
-    expect(alertMock).not.toBeNull();
-  })
+		clickAll(["Scanner", "Start"], screen.getByText);
 
-  it("find close button, click and check wheter modal is still opened", () => {
-    render(<ScannerPage />);
-    fireEvent.click(screen.getByText('Scanner'));
-    fireEvent.click(screen.getByText('Close'));
-    expect(screen.queryByTestId('close-button')).not.toBeInTheDocument();
-  })
-})
+		expect(screen.queryByTestId("video-elm")).toBeVisible();
+	});
+
+	it("send the result and receives the product info", async () => {
+		const alertMock = jest.spyOn(global, "alert");
+		render(
+			<FetchMock matcher="/product/123456" response={200} config={{ fallbackToNetwork: true }}>
+				<ScannerPage />
+			</FetchMock>
+		);
+		expect(alertMock).not.toBeNull();
+	});
+
+	it("find close button, click and check wheter modal is still opened", () => {
+		render(<ScannerPage />);
+
+		clickAll(["Scanner", "Close"], screen.getByText);
+
+		expect(screen.queryByTestId("close-button")).not.toBeInTheDocument();
+	});
+});
