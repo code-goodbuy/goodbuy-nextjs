88ca88bd1fd8c5911a3ed659a994f392223f5903
thatsddr <domenicodiruocco01@gmail.com>
2021-05-03 16:11:24 +0200

    removed duplication

--- cypress/e2e/login.ts
+++ cypress/e2e/login.ts
@@ -2,23 +2,23 @@
 /// <reference types="cypress" />
 
 describe("Test login", () => {
+	const expected = {
+		email: "goodbuytester@cuvox.de",
+		validPassword: "Password1!",
+		invalidPassword: "not a password"
+	};
+
 	it("Should log in a user", () => {
-		// given
-		cy.visit("/auth");
-		// when
-		cy.findFieldsAndType({ "Email": "goodbuytester@cuvox.de", "Password": "Password1!" });
-		cy.findByText("Log In").click();
+		// given + when
+		cy.formLogin(expected.email, expected.validPassword);
 		// then
 		cy.url().should("eq", Cypress.config().baseUrl + "/");
 		cy.get(".space-x-4 > .colorful-button").should("have.text", "Log Out");
 	});
 
 	it("Should display an error", () => {
-		// given
-		cy.visit("/auth");
-		// when
-		cy.findFieldsAndType({ "Email": "goodbuytester@cuvox.de", "Password": "not a password" });
-		cy.findByText("Log In").click();
+		// given + when
+		cy.formLogin(expected.email, expected.invalidPassword);
 		// then
 		cy.url().should("eq", Cypress.config().baseUrl + "/auth");
 		cy.findByText("An Error Occured");
