79dff6457088a397f86ae68e6a0152067b9c2695
thatsddr <domenicodiruocco01@gmail.com>
2021-05-06 11:06:24 +0200

    removed duplication in html

--- components/landingPage/Mission.tsx
+++ components/landingPage/Mission.tsx
@@ -2,6 +2,28 @@ import Picture from "../common/Picture";
 import InfoCard from "./InfoCard";
 
 export default function Mission() {
+	const data = [
+		{
+			number: 10000000,
+			unit: "Hectares",
+			text: "of forests lost since the beginning of this year.",
+			url: "http://www.fao.org/state-of-forests/en/"
+		},
+		{
+			number: 36000000000,
+			unit: "Tonnes",
+			text: "of CO2 emitted since the beginning of this year",
+			url: "https://ourworldindata.org/co2-emissions"
+		},
+		{
+			number: 170344000000000,
+			unit: "Liters",
+			text: "of water wasted since the beginning of this year",
+			url:
+				"https://www.npr.org/sections/thesalt/2013/06/06/189192870/when-you-waste-food-youre-wasting-tons-of-water-too"
+		}
+	];
+
 	return (
 		<div className="normal-bg flex flex-col h-auto lg:max-h-900" id="mission">
 			<h2 data-testid="mission-title" className="section-header">
@@ -9,30 +31,9 @@ export default function Mission() {
 			</h2>
 			<div className="flex flex-col justify-around lg:flex-row-reverse items-center pb-10 pt-10">
 				<div className="flex flex-col justify-around items-center lg:ml-10">
-					<InfoCard
-						number={10000000}
-						unit={"Hectares"}
-						text={"of forests lost since the beginning of this year."}
-						url={
-							"http://www.fao.org/state-of-forests/en/#:~:text=Between%202015%20and%202020%2C%20the,80%20million%20hectares%20since%201990."
-						}
-					/>
-					<InfoCard
-						number={36000000000}
-						unit={"Tonnes"}
-						text={"of CO2 emitted since the beginning of this year"}
-						url={
-							"https://ourworldindata.org/co2-emissions#:~:text=Emissions%20have%20continued%20to%20grow,36%20billion%20tonnes%20each%20year."
-						}
-					/>
-					<InfoCard
-						number={170344000000000}
-						unit={"Liters"}
-						text="of water wasted since the beginning of this year"
-						url={
-							"https://www.npr.org/sections/thesalt/2013/06/06/189192870/when-you-waste-food-youre-wasting-tons-of-water-too#:~:text=Now%20comes%20yet%20another%20reason,45%20trillion%20gallons%20of%20water."
-						}
-					/>
+					{data.map((i) => (
+						<InfoCard key={i.number} {...i} />
+					))}
 				</div>
 				<div className="flex flex-col items-center lg:mt-10 lg:max-w-1/2">
 					<Picture
