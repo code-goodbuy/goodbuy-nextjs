0e699a78e8c43abd71b177f826503cd891d26e1f
thatsddr <domenicodiruocco01@gmail.com>
2021-05-06 11:40:40 +0200

    removed old funcitons

--- lib/apiFunctions/jwtHelpers.ts
+++ lib/apiFunctions/jwtHelpers.ts
@@ -1,29 +1,6 @@
 import jwt_decode from "jwt-decode";
 import { JWTPayloadType } from "../../lib/types/HelperTypes";
 
-export const isValidJWT = (token: string): boolean => {
-	try {
-		jwt_decode<JWTPayloadType>(token);
-		return true;
-	} catch {
-		return false;
-	}
-};
-
-export const isExpiredJWT = (token: string): boolean => {
-	if (!isValidJWT(token)) return true;
-	const { exp } = jwt_decode<JWTPayloadType>(token);
-	if (exp && exp * 1000 >= Date.now()) return false;
-	return true;
-};
-
-export const decodeJWT = (token: string): JWTPayloadType | Error => {
-	if (!isExpiredJWT(token)) {
-		return jwt_decode<JWTPayloadType>(token);
-	}
-	throw new Error("Invalid / Expired JWT");
-};
-
 export class JWTHelper {
 	token: string;
 
@@ -41,14 +18,14 @@ export class JWTHelper {
 	}
 
 	isExpired(): boolean {
-		if (!isValidJWT(this.token)) return true;
+		if (!this.isValid()) return true;
 		const { exp } = jwt_decode<JWTPayloadType>(this.token);
 		if (exp && exp * 1000 >= Date.now()) return false;
 		return true;
 	}
 
 	decode(): JWTPayloadType | Error {
-		if (!isExpiredJWT(this.token)) {
+		if (!this.isExpired()) {
 			return jwt_decode<JWTPayloadType>(this.token);
 		}
 		throw new Error("Invalid / Expired JWT");
