5da205bb5ff8a44b08ad3e107d5bc5b086ecfddc
thatsddr <domenicodiruocco01@gmail.com>
2021-05-06 15:29:45 +0200

    deleted old functions

--- lib/apiFunctions/commonFunctions.ts
+++ lib/apiFunctions/commonFunctions.ts
@@ -68,7 +68,7 @@ export class APIHelper {
 
 	prepareForForwarding() {
 		this.config.req.headers.cookie = `jid=${this.tokens["refresh-token"]}`;
-		if (this.tokens["auth-token"] !== "") {
+		if (this.tokens["auth-token"] !== "" && this.tokens["auth-token"] !== undefined) {
 			this.config.req.headers["auth-token"] = this.tokens["auth-token"];
 			this.config.req.headers.Authorization = `Bearer ${this.tokens["refresh-token"]}`;
 		}
@@ -80,31 +80,37 @@ export class APIHelper {
 			autoRewrite: false,
 			selfHandleResponse: handleRes
 		};
-		return this.config.proxy.web(this.config.req, this.config.res, config, () => {
-			this.config.reject();
-		});
+		return (
+			this.config.proxy &&
+			this.config.proxy.web(this.config.req, this.config.res, config, () => {
+				this.config.reject();
+			})
+		);
 	}
 
-	handleResponse(handler: (body: string, proxyRes: IncomingMessage) => void) {
-		return this.config.proxy.once("proxyRes", (proxyRes, req, res) => {
-			let body = "";
-			proxyRes.on("data", (chunk: string) => {
-				body += chunk;
-			});
-			proxyRes.on("end", () => {
-				handler(body, proxyRes);
-			});
-		});
+	handleResponse(handler: "login" | "refresh") {
+		return (
+			this.config.proxy &&
+			this.config.proxy.once("proxyRes", (proxyRes, req, res) => {
+				let body = "";
+				proxyRes.on("data", (chunk: string) => {
+					body += chunk;
+				});
+				proxyRes.on("end", () => {
+					handler === "refresh" ? this.handleRefresh(body) : this.handleLogin(body, proxyRes);
+				});
+			})
+		);
 	}
 
 	handleLogin(body: string, proxyRes: IncomingMessage) {
 		const refreshToken = getTokenFromResponseCookie(proxyRes);
-		const jwt = getTokenFromResponse(body);
-		if (jwt && refreshToken) {
-			jwt && refreshToken && this.setAuthCookies(jwt, refreshToken);
-			this.resolveWithJWT(jwt);
+		const newToken = getTokenFromResponse(body);
+		if (newToken && refreshToken) {
+			newToken && refreshToken && this.setAuthCookies(newToken, refreshToken);
+			this.resolveWithJWT(newToken);
 		} else {
-			this.rejectIfCondition(jwt === null || refreshToken === undefined);
+			this.rejectIfCondition(newToken === null || refreshToken === undefined);
 		}
 	}
 
@@ -117,84 +123,4 @@ export class APIHelper {
 			this.rejectIfCondition(newToken === null);
 		}
 	}
-}
-
-export function rejectIfCondition(res: NextApiResponse, reject: () => void, condition: boolean) {
-	if (condition) {
-		res.status(409).json({ message: "error" });
-		reject();
-	}
-}
-
-export function getCommonRequirements(cookie: Cookies) {
-	const authToken = getTokenFromCookie(cookie, "auth-token");
-	const refreshToken = getTokenFromCookie(cookie, "refresh-token");
-	return { authToken, refreshToken };
-}
-
-export function resolveReq(res: NextApiResponse, resolve: () => void, message: {}) {
-	res.status(200).json(message);
-	resolve();
-}
-
-export function resolveIfValid({ token, response, resolve, message }: ResolveIfValidType) {
-	const isTokenValid = token && !new JWTHelper(token).isExpired();
-	if (isTokenValid) {
-		return resolveReq(response, resolve, { "message": message });
-	}
-}
-
-export function prepareForForwarding({ req, cookie = "", token = "" }: PrepareForForwardingType) {
-	req.headers.cookie = cookie;
-	if (token !== "") {
-		req.headers["auth-token"] = token;
-		req.headers.Authorization = `Bearer ${token}`;
-	}
-}
-
-export function forwardRequest({ req, res, proxy, handleRes, reject }: ForwardRequestType) {
-	const config = {
-		target: process.env.backendURL ? process.env.backendURL : "https:gb-be.de",
-		autoRewrite: false,
-		selfHandleResponse: handleRes
-	};
-	return proxy.web(req, res, config, () => {
-		reject();
-	});
-}
-
-export function setAuthCookies({ cookie, jwt, refreshToken }: setAuthCookiesType) {
-	setTokenCookie(cookie, "auth-token", jwt);
-	setTokenCookie(cookie, "refresh-token", refreshToken);
-}
-
-export function handleLogin({ req, res, proxyRes, body, resolve, reject }: HandleEndType) {
-	const refreshToken = proxyRes && getTokenFromResponseCookie(proxyRes);
-	const jwt = getTokenFromResponse(body);
-	rejectIfCondition(res, reject, jwt === null || refreshToken === undefined);
-	const cookie = initCookies(req, res);
-	jwt && refreshToken && setAuthCookies({ cookie, jwt, refreshToken });
-	const decoded = jwt && new JWTHelper(jwt).decode();
-	jwt && resolveReq(res, resolve, { ...decoded });
-}
-
-export function handleRefresh({ req, res, body, resolve, reject }: HandleEndType) {
-	const jwt = getTokenFromResponse(body);
-	rejectIfCondition(res, reject, jwt === null);
-	const cookie = initCookies(req, res);
-	jwt && setTokenCookie(cookie, "auth-token", jwt);
-	const decoded = jwt && new JWTHelper(jwt).decode();
-	jwt && resolveReq(res, resolve, { ...decoded });
-}
-
-export function handleResponse({ proxy, resolve, reject, handler }: HandleResponseType) {
-	return proxy.once("proxyRes", (proxyRes, req, res: any) => {
-		let body = "";
-		proxyRes.on("data", (chunk: string) => {
-			body += chunk;
-		});
-		proxyRes.on("end", () => {
-			handler({ req, res, proxyRes, body, resolve, reject });
-		});
-	});
 }
